bbb7dc3b1f165ad9f35f3df24d91414edaad4e3f
Simon O'Leary <spiritedlemon@hotmail.co.uk>
2019-05-04 18:00:17 +0100

    Working on NPC Pathing

---
+++ GE2Assignment/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+
+    This is used to identify which object is a waypoint by the NPCPathing script
+
+    */
+
+public class Waypoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
