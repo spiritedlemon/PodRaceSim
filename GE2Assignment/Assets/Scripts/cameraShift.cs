d5b316b603f4e4b21f6c9d767fed2a991a2cff2f
Simon O'Leary <spiritedlemon@hotmail.co.uk>
2019-05-06 17:56:41 +0100

    Changed while loop to if statement - fixed crash

--- GE2Assignment/Assets/Scripts/cameraShift.cs
+++ GE2Assignment/Assets/Scripts/cameraShift.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class cameraShift : MonoBehaviour
 {
+
+    public GameObject thisCam; //Camera script is attatched to
+    private Vector3 startpos; //Location camera will return to 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startpos = thisCam.transform.position; //save start point
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        while(this.GetComponent<Camera>().enabled == true)
+        if(thisCam.GetComponent<Camera>().enabled == true)
         {
 
             Debug.Log("Hey there");
         }
-
-        Debug.Log("now this");
+        
+        
 
     }
 }
