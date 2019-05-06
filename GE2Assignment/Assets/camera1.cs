2f07e6e1a959c6aa6d46c292e3965610ee7da60d
Simon O'Leary <spiritedlemon@hotmail.co.uk>
2019-05-06 17:48:28 +0100

    Bug causing program to freeze

--- GE2Assignment/Assets/camera1.cs
+++ GE2Assignment/Assets/camera1.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * This script is used to rotate the camera to look at 'pod9'
+ * This could be changed so the camera looks at the center point of the swarm but for the
+ * sake of story it will follow subulba
+ */
+
+
 public class camera1 : MonoBehaviour
 {
     public GameObject target;
---
+++ GE2Assignment/Assets/cameraShift.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraShift : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        while(this.GetComponent<Camera>().enabled == true)
+        {
+
+            Debug.Log("Hey there");
+        }
+
+        Debug.Log("now this");
+
+    }
+}
