912d8165aa736ad58ca7ab9ef4de3d0172aa9054
Simon O'Leary <spiritedlemon@hotmail.co.uk>
2019-05-06 15:56:39 +0100

    Added to eventManager script

---
+++ GE2Assignment/Assets/EventManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public int position;
+    public GameObject pod;
+
+    //Cameras
+    public GameObject one;
+    public GameObject two;
+    public GameObject three;
+    public GameObject four;
+    public GameObject five;
+    public GameObject six;
+    public GameObject seven;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = pod.GetComponent<FollowPath>().next;
+        
+        switch(position)
+        {
+            default:
+                break;
+            case 2:
+                //
+                break;
+                
+
+        }
+
+
+
+    }
+}
